e8b1f1accf9bfc89b2385cf4a0c009411b3b3e3d
kiraxe <kiraxe@yandex.ru>
2020-07-30 16:14:55 +0300

    miel 30.07.2020

--- resources/js/components/Public/Navbar/Nav/Links.jsx
+++ resources/js/components/Public/Navbar/Nav/Links.jsx
@@ -3,16 +3,25 @@ import {NavLink} from "react-router-dom";
 
 const Links = (props) => {
 
-    const [leftDropMenu, setLeftDropMenu] = useState(props.leftDropMenu);
+    const [leftDropMenu, setLeftDropMenu] = useState(false);
+    const [catId, setCatId] = useState(null);
 
-    useEffect(() => {
-        setLeftDropMenu(props.leftDropMenu);
-    });
+    const leftDropMenuHandler = page => {
+
+        setCatId(page);
+
+        if (leftDropMenu) {
+            setLeftDropMenu(false)
+        } else {
+            setLeftDropMenu(true)
+        }
+
+    }
 
     let className = leftDropMenu ? "active" : "";
 
     let categories = props.categories ? props.categories.map((item, key) => {
-        return  item.children.length > 0 ? <li className={'parent'} key={key}><NavLink onClick={props.leftDropMenuHandler} activeClassName="act" className={`leftDropMenuRun ${className}`} to={`/shop/${item.link}`} ><span>ГК «МИЭЛЬ»</span> {item.attributes.name}</NavLink><ul className={`leftDropMenu ${className}`}>{ props.categories.map((it, key) => it.parent_id === item.category_id ? <li key={key}><NavLink to={it.link} >{it.attributes.name}</NavLink></li> : null)}</ul></li> : !item.parent_id ? <li key={key}><NavLink to={`/shop/${item.link}`} >{item.attributes.name}</NavLink></li> : null
+        return  item.children.length > 0 ? <li className={'parent'} key={key}><NavLink onClick={() => leftDropMenuHandler(item_category_id)} activeClassName="act" className={`leftDropMenuRun ${catId === item.category_id ? className : ''}`} to={`/shop/${item.link}`} ><span>ГК «МИЭЛЬ»</span> {item.attributes.name}</NavLink><ul className={`leftDropMenu ${catId === item.category_id ? className : ''}`}>{ props.categories.map((it, key) => it.parent_id === item.category_id ? <li key={key}><NavLink to={`/shop/${it.link}`} >{it.attributes.name}</NavLink></li> : null)}</ul></li> : !item.parent_id ? <li key={key}><NavLink to={`/shop/${item.link}`} >{item.attributes.name}</NavLink></li> : null
     } ) : null;
 
     return(
