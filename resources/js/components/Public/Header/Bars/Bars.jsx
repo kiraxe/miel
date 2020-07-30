4a2b416b4aa98857ad40431ebe47f7884cfb63d7
kiraxe <kiraxe@yandex.ru>
2020-07-30 16:17:55 +0300

    miel 30.07.2020

--- resources/js/components/Public/Header/Bars/Bars.jsx
+++ resources/js/components/Public/Header/Bars/Bars.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Bars = (props) => {
     return (
         <div onClick={props.dropMenuHandler} className="col-xl-6 col-md-4 col-sm-4 col-2 bars">
-            <a href="#"><img src="images/bars.svg"/></a>
+            <a href="#"><img src="/images/bars.svg"/></a>
         </div>
     )
 }
