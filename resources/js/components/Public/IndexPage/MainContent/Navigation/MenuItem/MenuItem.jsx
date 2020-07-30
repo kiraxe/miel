54d7f4f9ebb2a9e0aa1a7b4b10a13906315df850
kiraxe <kiraxe@yandex.ru>
2020-07-30 16:06:09 +0300

    miel 30.07.2020

--- resources/js/components/Public/IndexPage/MainContent/Navigation/MenuItem/MenuItem.jsx
+++ resources/js/components/Public/IndexPage/MainContent/Navigation/MenuItem/MenuItem.jsx
@@ -5,7 +5,7 @@ const MenuItem = (props) => {
 
     return (
         <nav>
-            {props.categories ? props.categories.map((item, key) => !item.parent_id && item.main_menu ? <div key={key} className="menu-item"><div style={{backgroundImage: `url(${item.image})`}}><NavLink to={`shop/${item.link}`}><p className="title">{item.attributes.name}</p><p className="text">{item.attributes.description}</p></NavLink></div></div> : null) : null}
+            {props.categories ? props.categories.map((item, key) => item.main_menu ? <div key={key} className="menu-item"><div style={{backgroundImage: `url(${item.image})`}}><NavLink to={`shop/${item.link}`}><p className="title">{item.attributes.name}</p><p className="text">{item.attributes.description}</p></NavLink></div></div> : null) : null}
         </nav>
             )
 }
