3601e6f9980f64cd1d5535c05331655bd0a60056
kiraxe <kiraxe@yandex.ru>
2020-08-03 15:48:27 +0300

    miel 03.08.2020

--- resources/js/components/Public/CatalogPage/CatalogTitle/CatalogTitle.jsx
+++ resources/js/components/Public/CatalogPage/CatalogTitle/CatalogTitle.jsx
@@ -9,7 +9,7 @@ const CatalogTitle = (props) => {
 
     let cat = props.categories ? props.categories.filter(item => {
         let str = item.link = page.split('/');
-        console.log(str);
+        return str === page ? item : null;
     }) : null;
 
     return(
